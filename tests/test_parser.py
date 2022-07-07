4e77eabb8589783236f52cb159940939d6d03488
Torsten <tirlaender@aerosys.io>
2022-07-07 21:34:52 +0200

    feat(parser) Multiple terminals are supported.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -8,9 +8,15 @@ from babble.parser import BabbleTransformer
     "phrase,expected",
     [
         ("word", ["word"]),
+        ("word", ["word"]),
+        ("word word", ["word word"]),
+        ("'word' word", ["'word' word"]),
+        ("'word' word", ["'word' word"]),
+        ("'word123' word", ["'word123' word"]),
+        ("'word123' word word", ["'word123' word word"]),
     ],
 )
-def test_word(parser: Lark, transformer: BabbleTransformer, phrase: str, expected):
+def test_terminals(parser: Lark, transformer: BabbleTransformer, phrase: str, expected):
     tree = parser.parse(phrase)
     result = transformer.transform(tree)
     assert result == expected
